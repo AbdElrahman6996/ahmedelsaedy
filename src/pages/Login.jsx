b3c6038ef858467062618732671b0a9a3234f2f0
AbdElrahman <abdoamir6996@gmail.com>
2023-07-09 13:32:42 +0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Login/LoginHeader'
 import Form from '../components/Login/Form'
+import Alert from '../components/Login/Alert'
 
 const Login = () => {
   return (
     <>
         <Header/>
         <Form/>
+        <Alert/>
     </>
   )
 }
